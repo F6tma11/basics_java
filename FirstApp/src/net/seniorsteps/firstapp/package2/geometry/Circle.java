42deb8d7979846b0c02b96fbc68958182a9670b5
Your Name <you@example.com>
2024-10-27 19:23:28 +0300

    Add java project for basices

---
+++ FirstApp/src/net/seniorsteps/firstapp/package2/geometry/Circle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.seniorsteps.firstapp.package2.geometry;
+
+/**
+ *
+ * @author fatma
+ */
+public class Circle {
+    private float distance;
+    
+    public void setDistance(float d){
+        distance=d;
+    }
+    
+    public float getDistance(){
+        return distance;
+    }
+    
+}
